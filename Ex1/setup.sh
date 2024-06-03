b8788737485c3c46479a10d1ca0342ca9b886b6f
Aviv jan <avivjan2000@gmail.com>
2024-06-03 18:00:02 +0300

    platform

--- Ex1/setup.sh
+++ Ex1/setup.sh
@@ -3,15 +3,29 @@
 # Function to install Miniconda
 install_miniconda() {
     echo "Conda could not be found. Installing Miniconda..."
-    # Download Miniconda installer
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda3-latest.sh
-    # Run the installer
-    bash Miniconda3-latest.sh -b -p $HOME/miniconda
+
+    # Determine the platform and download the appropriate installer
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
+        INSTALLER="Miniconda3-latest-MacOSX.sh"
+        wget $MINICONDA_URL -O $INSTALLER
+        bash $INSTALLER -b -p $HOME/miniconda
+    elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+        MINICONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
+        INSTALLER="Miniconda3-latest-Windows.exe"
+        curl -O $MINICONDA_URL -o $INSTALLER
+        start /wait "" $INSTALLER /InstallationType=JustMe /AddToPath=1 /RegisterPython=0 /S /D=%UserProfile%\miniconda
+    else
+        echo "Unsupported platform: $OSTYPE"
+        exit 1
+    fi
+
     # Initialize conda
-    eval "$($HOME/miniconda/bin/conda shell.bash hook)"
+    eval "$($HOME/miniconda/bin/conda shell.bash hook)" || eval "$(%UserProfile%\\miniconda\\Scripts\\conda shell.bash hook)"
     conda init
+
     # Remove installer
-    rm Miniconda3-latest.sh
+    rm $INSTALLER
     echo "Miniconda installed successfully."
 }
 
